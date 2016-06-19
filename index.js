a30d5b41224fdb241caf58079ba6e5c58b84bbba
alex-zer0 <alex1tut@mail.ru>
2016-06-19 20:38:46 +0300

    Index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 module.exports = {
     configs: {
-        errors: require(".eslint/errors"),
-        'best-practices': require(".eslint/best-practices"),
-        strict: require(".eslint/strict"),
-        variables: require(".eslint/variables"),
-        style: require(".eslint/style"),
-        es6: require(".eslint/es6"),
-        flowtype: require(".eslint/flowtype"),
-        mocha: require(".eslint/mocha"),
-        react: require(".eslint/react")
+        errors: require("./.eslint/errors"),
+        'best-practices': require("./.eslint/best-practices"),
+        strict: require("./.eslint/strict"),
+        variables: require("./.eslint/variables"),
+        style: require("./.eslint/style"),
+        es6: require("./.eslint/es6"),
+        flowtype: require("./.eslint/flowtype"),
+        mocha: require("./.eslint/mocha"),
+        react: require("./.eslint/react")
     }
 };
